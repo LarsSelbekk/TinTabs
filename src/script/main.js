54d11cef68c5117c4fd6ba3c492e60a31a8733d5
Lars Mitsem Selbekk <lars@selbekk.com>
2020-05-27 22:45:09 +0200

    Create better MVP

--- src/script/main.js
+++ src/script/main.js
@@ -1,48 +1,17 @@
-const ol_tabs = document.getElementById("ol_tabs");
-const inp_noteString = document.getElementById("inp_noteString");
+"use strict";
+const dropCheatSheet = document.getElementById("dropCheatSheet");
+const cheatsheet = document.getElementById("cheatsheet");
+dropCheatSheet.addEventListener("click", toggleCheatSheet);
 
-const validNotes = ["d", "e", "f#", "g", "a", "b", "c#", "D", "E", "F#", "G", "A", "B", "C#"];
-const validNotesWithSharp = ["f", "c", "F", "C"];
+let cheatSheetDown = false;
 
-const notes = [];
-
-inp_noteString.addEventListener("input", updateTabs);
-
-function updateTabs() {
-    const noteString = inp_noteString.value;
-    let i = 0;
-    while (i < noteString.length) {
-	const letter = noteString[i++];
-	if (validNotes.contains(letter)) {
-	    addNote(letter);
-	} else if (validNotesWithSharp.contains(letter)) {
-	    const modifier = noteString[i++];
-	    if (validNotes.contains(letter + modifier)) {
-		addNote(letter + modifier);
-	    } else {
-		return;
-	    }
-	} else {
-	    return;
-	}
+function toggleCheatSheet() {
+    if (cheatSheetDown) {
+        dropCheatSheet.innerText = 'Cheat sheet \u25bc';
+        cheatsheet.classList.add("hidden");
+    } else {
+        dropCheatSheet.innerText = 'Cheat sheet \u25b2';
+        cheatsheet.classList.remove("hidden");
     }
-    updateTabs();
-}
-
-function addNote(note) {
-    notes.put(note);
-}
-
-function updateTabs() {
-    ol_tabs.innerHTML = "";
-    notes.forEach(
-	note => ol_notes.appendChild(generateNoteElement(note)));
-}
-
-function generateNoteElement(note) {
-    const li = document.createElement("li");
-    const img = document.createElement("img");
-    img.src = "../assets/notes/note_${note}.png";
-    li.appendChild(img);
-    return li;
+    cheatSheetDown = !cheatSheetDown;
 }
