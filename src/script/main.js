f61f4a48885198b698a72410d60a87d1be7042eb
Lars Mitsem Selbekk <lars@selbekk.com>
2020-05-28 03:40:08 +0200

    Make usable

--- src/script/main.js
+++ src/script/main.js
@@ -1,16 +1,57 @@
 "use strict";
 const dropCheatSheet = document.getElementById("dropCheatSheet");
 const cheatsheet = document.getElementById("cheatsheet");
+const tabs = document.getElementById("tabs");
+
+const lastNoteKeyStandIn = "K";
+const lastNoteKey = "\u00ce";
+
+const allowedNotes = [
+    "d", "i", "e", "j", "f", "g", "h", "a", "n", "b", "m", ",", "c", " ", "Enter",
+    "D", "I", "E", "J", "F", "G", "H", "A", "N", "B", "M", "C", lastNoteKeyStandIn
+];
+
 dropCheatSheet.addEventListener("click", toggleCheatSheet);
+
+tabs.addEventListener("focusout", e => {
+    const target = e.currentTarget;
+    if (target.textContent.trim().length === 0) {
+        target.textContent = "";
+    }
+});
+
+tabs.addEventListener("keypress", e => {
+    const key = e.key;
+    if (allowedNotes.indexOf(key) < 0) {
+        e.preventDefault();
+    }
+    else if (key === lastNoteKeyStandIn) {
+        interceptKey(e, lastNoteKey);
+    }
+});
+
+function interceptKey(event, replacementString) {
+    let selection = window.getSelection();
+    let startIndex = Math.min(selection.focusOffset, selection.anchorOffset);
+    let endIndex = Math.max(selection.focusOffset, selection.anchorOffset);
+    let oldContent = event.target.textContent;
+
+    event.target.textContent = oldContent.slice(0, startIndex) + replacementString + oldContent.slice(endIndex);
+
+    selection.setPosition(event.currentTarget.firstChild, startIndex+1);
+
+    event.preventDefault();
+}
 
 let cheatSheetDown = false;
 
 function toggleCheatSheet() {
     if (cheatSheetDown) {
-        dropCheatSheet.innerText = 'Cheat sheet \u25bc';
+        dropCheatSheet.value = "Cheat sheet \u25bc";
         cheatsheet.classList.add("hidden");
-    } else {
-        dropCheatSheet.innerText = 'Cheat sheet \u25b2';
+    }
+    else {
+        dropCheatSheet.value = "Cheat sheet \u25b2";
         cheatsheet.classList.remove("hidden");
     }
     cheatSheetDown = !cheatSheetDown;
